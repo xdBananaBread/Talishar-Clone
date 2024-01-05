fb001ed6a001eb0c2ff217b04d95d54b46629198
xdBananaBread <155521696+xdBananaBread@users.noreply.github.com>
2024-01-05 01:48:10 -0800

    Update APIKeys.php

--- APIKeys/APIKeys.php
+++ APIKeys/APIKeys.php
@@ -1,7 +1,7 @@
 <?php
 
 // Fabrary
-$FabRaryKey = "flesh-and-blood-online-moa4z4eauZVR8Zfdx7i7yAsutRcuvL";
+$FaBraryKey = "flesh-and-blood-online-moa4z4eauZVR8Zfdx7i7yAsutRcuvL";
 $fabraryOutgoingKey = "op://$APP_ENV/Fabrary/OutgoingKey";
 
 // Sendgrid
