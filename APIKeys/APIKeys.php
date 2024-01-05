f61e416c722f382faccb790f14b27c1def46bc92
xdBananaBread <155521696+xdBananaBread@users.noreply.github.com>
2024-01-05 02:35:56 -0800

    Update APIKeys.php

--- APIKeys/APIKeys.php
+++ APIKeys/APIKeys.php
@@ -3,22 +3,3 @@
 // Fabrary
 $FaBraryKey = "flesh-and-blood-online-moa4z4eauZVR8Zfdx7i7yAsutRcuvL2";
 $fabraryOutgoingKey = "op://$APP_ENV/Fabrary/OutgoingKey";
-
-// Sendgrid
-$sendgridKey = "op://$APP_ENV/Sendgrid/credential";
-
-// Adsense - Not used
-$adsenseClientId = "op://$APP_ENV/Adsense/credential";
-
-// FABDB
-$fabDBToken = "op://$APP_ENV/FabDB/token";
-$fabDBSecret = "op://$APP_ENV/FabDB/secret";
-
-// Patreon
-$patreonClientID = "op://$APP_ENV/Patreon/username";
-$patreonClientSecret = "op://$APP_ENV/Patreon/credential";
-
-// SSH for loading bugged games
-$prodServerIP = "op://$APP_ENV/Production Server/URL";
-$prodServerUsername = "op://$APP_ENV/Production Server/username";
-$prodServerPassword = "op://$APP_ENV/Production Server/password";
