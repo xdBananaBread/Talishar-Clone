8efe5b70688328682e073aabb39da0c6d513f68f
xdBananaBread <155521696+xdBananaBread@users.noreply.github.com>
2024-01-08 15:16:04 -0800

    Update PlayAbilities.php

--- CardDictionaries/PlayAbilities.php
+++ CardDictionaries/PlayAbilities.php
@@ -159,6 +159,7 @@
           AddDecisionQueue("MULTICHOOSETEXT", $otherPlayer, "1-Might (+1),Vigor (Resource),Quicken (Go Again)-1");
           AddDecisionQueue("SHOWMODES", $otherPlayer, $cardID, 1);
           AddDecisionQueue("MODAL", $otherPlayer, "JINGLEWOOD", 1);
+          PutItemIntoPlayForPlayer($cardID, $otherPlayer);
           PutItemIntoPlayForPlayer("CRU197", $currentPlayer);
           --$character[$charIndex+5];
           }
